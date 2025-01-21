6f865c71f3cd959d5aa23d874e2fe443023fccdb
Gabriel Karing <gabhenkar@gmail.com>
2025-01-21 16:27:03 -0300

    New Commit

---
+++ Aula10/Aula10.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Aula10{
+
+    class Aula10{
+
+        public static void Main(){
+
+            float n1 = 10.5f;
+            int n2 =(int)n1;// implicit conversion
+
+
+            int vInt = 10;
+            short vShort = (short)vInt; // explicit conversion           
+            Console.WriteLine(vShort);
+            Console.WriteLine(n2);
+
+        }
+
+    }
+}
